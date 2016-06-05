8e66a94eab349e3a18f2217239abbc8d97664503
Kim Siang Khaw <khaw84@uw.edu>
2016-06-05 14:00:47 -0700

    fixed file closing problem in main.cxx

--- SLAC2016Ana/main.cxx
+++ SLAC2016Ana/main.cxx
@@ -8,7 +8,7 @@
 
 int main (int argc, char **argv) {
 
-    TCanvas *mydummycanvas = new TCanvas(); 
+    // TCanvas *mydummycanvas = new TCanvas(); 
 
     string temp[100];
     cout << "###############################################################" << endl;
@@ -24,49 +24,52 @@ int main (int argc, char **argv) {
 
     while ( !inData.eof() )
     {
-        getline (inData, line); //get contents of infile 
-        cout << line << endl; //output contents of infile   
-        size++;
+	getline (inData, line); //get contents of infile 
+	cout << line << endl; //output contents of infile   
+	size++;
     } 
 
+    TChain *chain = new TChain ("slacAnalyzer/eventTree");
 
     for(unsigned int j=1;j<size;j++){
-        TChain *chain = new TChain ("slacAnalyzer/eventTree");
-        TString fname;
-        if (argc > 1)
-        {
-            ifstream fin (argv[1]);
-            if (!fin.is_open ())
-            {
-                cerr << "file open error:" << argv[1] << endl;
-                exit (1);
-            }
-            unsigned int i = 0;
-            while (fin >> fname)
-            {
-                i++;
-                if(i==j){
-                    chain->Add (fname);
-                    temp[i] = fname;
-                    int n = temp[i].rfind ("/"); // find the position in string of the last "/"
-                    temp[i].erase (0, n + 1);	// erase substring in front of it including itself
-                    // cout << temp[i] << endl; // print the input rootfile name
-                }
-            }
-        }
-        else
-        {
-            cerr << "no input file" << endl;
-            exit (1);
-        }
-        SLAC2016Ana ana(chain);
+	TString fname;
+	if (argc > 1)
+	{
+	    ifstream fin (argv[1]);
+	    if (!fin.is_open ())
+	    {
+		cerr << "file open error:" << argv[1] << endl;
+		exit (1);
+	    }
+	    unsigned int i = 0;
+	    while (fin >> fname)
+	    {
+		i++;
+		if(i==j){
+		    chain->Add (fname);
+		    temp[i] = fname;
+		    int n = temp[i].rfind ("/"); // find the position in string of the last "/"
+		    temp[i].erase (0, n + 1);	// erase substring in front of it including itself
+		    // cout << temp[i] << endl; // print the input rootfile name
+		}
+	    }
 
-        cout <<"###################################################################"<< endl;
-        cout <<"##    Current file is : "<< temp[j] << "   ##"<<endl; // print the input rootfile name
-        cout <<"###################################################################"<< endl;
+	    fin.close();   
+	}
 
-        ana.Loop(temp[j]);
+	else
+	{
+	    cerr << "no input file" << endl;
+	    exit (1);
+	}
+
+	cout <<"###################################################################"<< endl;
+	cout <<"##    Current file is : "<< temp[j] << "   ##"<<endl; // print the input rootfile name
+	cout <<"###################################################################"<< endl;
+
     }
 
+    SLAC2016Ana ana(chain);
+    ana.Loop(temp[0]);
     return 0;
 }
