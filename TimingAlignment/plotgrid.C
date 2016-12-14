0622ec7162b645464ebf5b3bb3cb4c0b0919a7f7
kimsiang <kimsiang84@gmail.com>
2016-12-13 17:23:07 -0800

    updated timing offset plotting macro

--- TimingAlignment/plotgrid.C
+++ TimingAlignment/plotgrid.C
@@ -9,25 +9,24 @@ void plotgrid(){
 
     ifstream file("grid.txt");
 
-    TCanvas *c1 = new TCanvas("c1","c1",800,600);
+    TCanvas *c1 = new TCanvas("c1","c1",1600,600);
+    
     TH2D *grid = new TH2D("grid","grid",54,0,54,54,0,54);
     TH1D *h_dt = new TH1D("dt","dt",100,-2,2);
 
     while(file>>i[n]>>j[n]>>dt[n]){
-/*        double a=roundf(i[n]*100)/100;
-        double b=roundf(j[n]*100)/100;
-        double c=roundf(dt[n]*100)/100;
-  */      h_dt->Fill(dt[n]);
-   //   grid->Fill(i[n],j[n],1);
-        n++;
+	h_dt->Fill(dt[n]);
+	grid->Fill(i[n],j[n],1);
+	n++;
     }
 
-    //   c1->SetGridx();
-    //    c1->SetGridy();
-  //grid->Draw("text colz");
+    c1->Divide(2,1);
+    c1->cd(1);
+    c1->cd(1)->SetGridx();
+    c1->cd(1)->SetGridy();
+    grid->Draw("text colz");
+    c1->cd(2);
     h_dt->Draw();
     c1->Print("grid.pdf");
-
-    
 
 }
