c31cfe25e4ef331b9f74d86e069d0209c74355fa
kimsiang <kimsiang84@gmail.com>
2016-12-16 14:03:44 -0800

    upated crossFitter

--- CrossFittingStudy/srcs/crossFitter.cxx
+++ CrossFittingStudy/srcs/crossFitter.cxx
@@ -7,6 +7,7 @@
 #include "TSpline.h"
 #include "TRandom.h"
 #include "TCanvas.h"
+#include "TStyle.h"
 #include "TGraph.h"
 #include "TPaveText.h"
 #include "TString.h"
@@ -17,6 +18,7 @@
 #include "TColor.h"
 
 #include <iostream>
+#include <stdlib.h>
 #include <iomanip>
 #include <algorithm>
 #include <cmath>
@@ -27,22 +29,48 @@
 
 using namespace std;
 
-bool drawing = false;
+bool drawing = true;
 
 gm2calo::FitResultArtRecord fitWaveform(TemplateFitter& tf, const gm2calo::IslandArtRecord, TSpline3* tSpline);
 
 int main(int argc, char const* argv[]) {
 
-    if(argc > 1){
-	cout<<"There should be no argument!!"<<endl;
-	return 1;
+    if(argc < 2){
+        cout<<"Usage ./crossFitter nTry"<<endl;
+        return 1;
     }
 
     // declare new TApplication
     new TApplication("app", 0, nullptr);
+    gStyle->SetLabelSize(0.05,"xy");
+    gStyle->SetTitleSize(0.05,"xy");
 
-    TString splinePath = "beamTemplateFile37.root";
-    TString splinePath2 = "beamleakTemplateFile37.root";
+    TString splinePath;
+    TString splinePath2;
+
+    if(atoi(argv[2]) == 1){
+        splinePath = "beamTemplateFile37.root";
+    }
+    else if(atoi(argv[2]) == 2){
+        splinePath = "beamleakTemplateFile37.root";
+    }
+    else{
+        cout<<"Please use only template 1(A) or 2(B)!"<<endl;
+        return 2;
+    }
+
+    if(atoi(argv[3]) == 1){
+        splinePath2 = "beamTemplateFile37.root";
+    }
+    else if(atoi(argv[3]) == 2){
+        splinePath2 = "beamleakTemplateFile37.root";
+    }
+    else{
+        cout<<"Please use only sample 1(A) or 2(B)!"<<endl;
+        return 3;
+    }
+
+
 
     // get TSpline for main shower
     TFile templateFile(splinePath);
@@ -61,46 +89,68 @@ int main(int argc, char const* argv[]) {
     vector<short> trace;
 
     // Loop through some randomized energies
-    int nTry=10000;
+    int nTry = atoi(argv[1]);
 
     // 2D histogram to compare input and output
-    TH2D *compareE = new TH2D("compareE","compareE",100,999,1001,100,999,1001);
-    TH2D *compareT = new TH2D("compareT","compareT",100,9.9,10.1,100,9.9,10.1);
-    compareE->GetXaxis()->SetCanExtend(kTRUE);
-    compareE->GetYaxis()->SetCanExtend(kTRUE);
-    compareT->GetXaxis()->SetCanExtend(kTRUE);
-    compareT->GetYaxis()->SetCanExtend(kTRUE);
-    
+    TH2D *compareE = new TH2D("compareE","E_{fitted} vs E_{input}",500,0,8000,500,0,8000);
+    TH2D *compareT = new TH2D("compareT","T_{fitted} vs T_{input}",500,9.5,10.5,500,9.5,10.5);
+    compareE->GetXaxis()->SetTitle("E_{input}");
+    compareE->GetYaxis()->SetTitle("E_{fitted}");
+    compareT->GetXaxis()->SetTitle("T_{input}");
+    compareT->GetYaxis()->SetTitle("T_{fitted}");
+
+    //   compareE->GetYaxis()->SetCanExtend(kTRUE);
+    //   compareT->GetYaxis()->SetCanExtend(kTRUE);
+
+    TH2D *timeDiff = new TH2D("timeDiff","T_{fitted-input} vs E_{input}",500,0,8000,500,-0.06,0.06); 
+    TH2D *energyRatio = new TH2D("energyRatio","(E_{fitted}-E_{input})/E_{input} vs E_{input}",500,0,8000,500,-0.06,0.06); 
+    timeDiff->GetXaxis()->SetTitle("E_{input}");
+    timeDiff->GetYaxis()->SetTitle("E_{input} vs T_{fitted-input}");
+    energyRatio->GetXaxis()->SetTitle("E_{input}");
+    energyRatio->GetYaxis()->SetTitle("(E_{fitted}-E_{input})/E_{input}");
+
+    //   timeDiff->GetYaxis()->SetCanExtend(kTRUE);
+    //   energyRatio->GetYaxis()->SetCanExtend(kTRUE);
 
     for(int iTry=0;iTry<nTry;iTry++){
 
-	// empty trace
-	trace.clear();
+        // empty trace
+        trace.clear();
 
-	// generate a random time
-	double time = 10 + (gRandom->Rndm() - 0.5);
+        // generate a random time and energy
+        double time = 10 + (gRandom->Rndm() - 0.5);
+        double energy = 7000 * (0.05 + gRandom->Rndm());
 
-	for(int i=0;i<40;i++){
-	    trace.push_back(1000*tSpline->Eval(i-time));
-	}
+        for(int i=0;i<40;i++){
+            trace.push_back(energy*tSpline2->Eval(i-time));
+        }
 
-	// store the waveform inside an IslandArtRecord
-	gm2calo::IslandArtRecord thisIsland; 
-	thisIsland.trace = trace;
-	thisIsland.firstSampleNum = 0;;
+        // store the waveform inside an IslandArtRecord
+        gm2calo::IslandArtRecord thisIsland; 
+        thisIsland.trace = trace;
+        thisIsland.firstSampleNum = 0;
 
-	auto fitResults = fitWaveform(tf, thisIsland, tSpline.get()); 
+        auto fitResults = fitWaveform(tf, thisIsland, tSpline.get()); 
 
-	compareE->Fill(1000,fitResults.energy);
-	compareT->Fill(time,fitResults.time);
+        compareE->Fill(energy,fitResults.energy);
+        compareT->Fill(time,fitResults.time);
+        energyRatio->Fill(energy, (fitResults.energy-energy)/energy );
+        timeDiff->Fill(energy, (fitResults.time-time) );
+
     }
 
-    TCanvas c1("c1","c1",1200,600);
-    c1.Divide(2,1);
+    TCanvas c1("c1","c1",1200,800);
+    c1.Divide(2,2);
     c1.cd(1);
     compareE->Draw("colz");
     c1.cd(2);
     compareT->Draw("colz");
+    c1.cd(3);
+    energyRatio->Draw("colz");
+    c1.cd(4);
+    timeDiff->Draw("colz");
+
+
     c1.Modified();
     c1.Update();
     c1.Draw();
@@ -115,8 +165,8 @@ gm2calo::FitResultArtRecord fitWaveform(TemplateFitter& newFitter, const gm2calo
     // make plot
     std::string title = "Test";
 
-//   std::unique_ptr<TCanvas> c(
-//	    new TCanvas("c", (title + "_canvas").c_str(), 1200, 1200));
+    std::unique_ptr<TCanvas> c(
+            new TCanvas("c", (title + "_canvas").c_str(), 1200, 1200));
 
     std::unique_ptr<TGraph> g(new TGraph(0));
 
@@ -124,11 +174,11 @@ gm2calo::FitResultArtRecord fitWaveform(TemplateFitter& newFitter, const gm2calo
 
     vector<short> sampleTimes;
     for(size_t i=0;i<trace.size();i++){
-	sampleTimes.push_back(i + thisIsland.firstSampleNum);
+        sampleTimes.push_back(i + thisIsland.firstSampleNum);
     }
 
     for (size_t i = 0; i < trace.size(); ++i) {
-	g->SetPoint(g->GetN(), sampleTimes[i], trace[i]);
+        g->SetPoint(g->GetN(), sampleTimes[i], trace[i]);
     }
 
     gm2calo::TemplateFitApplier applyTemplateFit_(false, 20, 2, 10, 0.025, 30, 3);
@@ -137,20 +187,20 @@ gm2calo::FitResultArtRecord fitWaveform(TemplateFitter& newFitter, const gm2calo
     double xMin = tSpline->GetXmin();
     double xMax = tSpline->GetXmax();
     auto templateFunction = [&](double* x, double* p) {
-	double returnValue = p[6];
-	if ((x[0] - p[0] > xMin) && (x[0] - p[0] < xMax)) {
-	    returnValue += p[1] * tSpline->Eval(x[0] - p[0]);
-	}
-	return returnValue;
+        double returnValue = p[6];
+        if ((x[0] - p[0] > xMin) && (x[0] - p[0] < xMax)) {
+            returnValue += p[1] * tSpline->Eval(x[0] - p[0]);
+        }
+        return returnValue;
     };
 
     std::unique_ptr<TF1> func(new TF1("fitFunc", templateFunction, sampleTimes[0],
-		sampleTimes.back(), 7));
+                sampleTimes.back(), 7));
     func->SetLineColor(kBlack);
     func->SetParameters(std::vector<double>(7, 0).data());
 
     g->SetMarkerStyle(20);
-//   g->Draw("ap");
+    g->Draw("ap");
     g->GetXaxis()->SetRangeUser(sampleTimes[0], sampleTimes.back());
     g->GetXaxis()->SetTitle("sample number");
     g->GetYaxis()->SetTitle("ADC counts");
@@ -162,8 +212,8 @@ gm2calo::FitResultArtRecord fitWaveform(TemplateFitter& newFitter, const gm2calo
     double xRangeMax = sampleTimes.back();
 
     std::unique_ptr<TPaveText> txtbox(new TPaveText(
-		xRangeMin + (xRangeMax - xRangeMin) * 0.6, yMin + (yMax - yMin) * 0.6,
-		xRangeMin + (xRangeMax - xRangeMin) * 0.9, yMin + (yMax - yMin) * 0.35));
+                xRangeMin + (xRangeMax - xRangeMin) * 0.6, yMin + (yMax - yMin) * 0.6,
+                xRangeMin + (xRangeMax - xRangeMin) * 0.9, yMin + (yMax - yMin) * 0.35));
     txtbox->SetFillColor(kWhite);
     func->SetParameter(6, newFitResults[0].pedestal);
     func->SetParameter(0, newFitResults[0].time);
@@ -174,17 +224,17 @@ gm2calo::FitResultArtRecord fitWaveform(TemplateFitter& newFitter, const gm2calo
     txtbox->AddText(Form("#chi^{2} / NDF : %.2f", newFitResults[0].chi2));
 
     func->SetNpx(10000);
-//   func->Draw("same");
+    func->Draw("same");
     func->SetLineColor(kMagenta + 2);
-//    txtbox->Draw("same");
+    txtbox->Draw("same");
 
-/*    if(drawing){
-	c->Modified();
-	c->Update();
-	c->Draw();
-	gSystem->ProcessEvents();
-	//cin.ignore();
+    if(drawing){
+        c->Modified();
+        c->Update();
+        c->Draw();
+        gSystem->ProcessEvents();
+        cin.ignore();
     }
-*/
+
     return newFitResults[0];
 }
