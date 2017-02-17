ee196290a606fdd270508d8c5b552260ead39e70
kimsiang <kimsiang84@gmail.com>
2017-02-16 23:07:59 -0800

    added a new folder titled AnalysisCode

---
+++ AnalysisCode/include/GeneralAnalysis.h
@@ -0,0 +1,52 @@
+#ifndef GeneralAnalysis_h
+#define GeneralAnalysis_h
+
+#include "EventCollection.h"
+#include <stdio.h>
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include <iostream>
+#include <fstream>
+#include <map>
+
+class GeneralAnalysis : public EventCollection {
+  public:
+    GeneralAnalysis(TTree *tree=0):EventCollection(tree){};
+    ~GeneralAnalysis() {};
+    virtual void Loop(string filename);
+    void initialize(string &filename);
+    void execute();
+    void finalize(string &filename);
+    void clear();
+
+  private:
+    TFile *file;     
+    TH1D *syncTime[54];       
+    TH1D *fittedSyncTime[54];       
+    TH1D *syncEnergy[54];       
+    TH2D *syncTimeEnergy[54];       
+    TH1D *syncTimeDiff[54][54];       
+
+    TH1D *fittedBeamTime[54];       
+    TH1D *fittedBeamTimeDiff[54][54];       
+
+    TH1D *beamTime[54];       
+    TH1D *beamEnergy[54];       
+    TH2D *beamTimeEnergy[54];       
+    TH1D *beamTimeDiff[54][54];               
+
+    TH2D *syncTime2D;
+    TH2D *syncEnergy2D;
+    TH2D *beamTime2D;
+    TH2D *beamEnergy2D;
+
+    TH2D *beamTimeDiffEnergy[54][54];
+    TH2D *syncTimeDiffEnergy[54][54];
+
+    TH1D *chi2;
+    TH2D *energyChi2;
+};
+#endif
