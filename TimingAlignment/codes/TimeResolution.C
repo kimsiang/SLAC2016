db41b00e9c905bfab8b805635505734859f14af9
kimsiang <kimsiang84@gmail.com>
2017-05-14 14:54:11 -0700

    memory optimized TimeResolution.C

--- TimingAlignment/codes/TimeResolution.C
+++ TimingAlignment/codes/TimeResolution.C
@@ -41,12 +41,15 @@ bool isSameWFD5(int ch1, int ch2){
   return same;
 }
 
+
 void TimeResolution(){
 
-  // set important const variables here 
-  int startRun = 2133;
+ROOT::EnableThreadSafety();
+
+// set important const variables here 
+  int startRun = 2133; // 1751 or 2133 for now
   const int nRun = 8;
-  const int nXtal = 10;
+  const int nXtal = 54;
 
   // files to open
   TFile *f[nRun];
@@ -61,7 +64,6 @@ void TimeResolution(){
   double effEnergy[nXtal][nXtal][nRun];
   TH1D *syncE[nXtal][nRun];
   TH1D *syncTimeDiff[nXtal][nXtal][nRun];
-  TF1 *fit;
 
   // plotting E and dt distributions for sanity check
   // TCanvas *c[8];
@@ -69,6 +71,7 @@ void TimeResolution(){
   // loop for each run
   for(int i=0;i<nRun;i++){
 
+      cout<<"Run: "<<i+startRun<<endl;
     // c[i] = new TCanvas(Form("c%d",i),Form("c%d",i),1200,900);
     // c[i]->Divide(9,6);
 
@@ -76,32 +79,28 @@ void TimeResolution(){
     for(int j=0;j<nXtal;j++){
 
       // c[i]->cd(54-j);
-      syncE[j][i] = (TH1D*) f[i]->Get(Form("syncEnergy%02d",j));
+      f[i]->GetObject(Form("syncEnergy%02d",j), syncE[j][i]);
 
       // get initial parameters for the energy fit
       double mean = syncE[j][i]->GetBinCenter(syncE[j][i]->GetMaximumBin());
       double rms = syncE[j][i]->GetRMS();
-
-      // now fit energy, 1st trial
-      fit = new TF1("fit","gaus(0)",mean-3*rms,mean+3*rms);
-      fit->SetParameters(500,mean,rms);
+      
+      TF1 fit("fit","gaus(0)",mean-3*rms,mean+3*rms);
+      fit.SetParameters(500,mean,rms);
       syncE[j][i]->Fit("fit","QMERN");
-      mean = fit->GetParameter(1);
-      rms = fit->GetParameter(2);
-      fit->Clear(); 
-
-      // now fit energy, 2nd trial
-      fit = new TF1("fit","gaus(0)",mean-3*rms,mean+3*rms);
-      fit->SetParameters(500,mean,rms);
+     
+      mean = fit.GetParameter(1);
+      rms = fit.GetParameter(2);
+      fit.SetParameters(500,mean,rms);
       syncE[j][i]->Fit("fit","QMERN");
-      mean = fit->GetParameter(1);
-      rms = fit->GetParameter(2);
-      fit->Clear(); 
-
+      
+      mean = fit.GetParameter(1);
+      rms = fit.GetParameter(2);
+      
       // get pixel saturation corrected energy
       energy[j][i] = eReal(mean);
 
-      cout<<"Run: "<<i+startRun<<", Xtal: "<<j<<", E: "<<mean<<"("<<fit->GetChisquare()/fit->GetNDF()<<")"<<endl;
+      cout<<"  Xtal: "<<j<<", E: "<<mean<<" (Chi2/NDF: "<<fit.GetChisquare()<<"/"<<fit.GetNDF()<<")"<<endl;
 
       // loop for each crystal to compare with
       for(int k=0;k<nXtal;k++){
@@ -109,7 +108,7 @@ void TimeResolution(){
         // (j,k) is the same with (k,j)
         if(k>=j) continue;
 
-        syncTimeDiff[k][j][i] = (TH1D*) f[i]->Get(Form("syncTimeDiff%02d%02d",j,k));
+        f[i]->GetObject(Form("syncTimeDiff%02d%02d",j,k),syncTimeDiff[k][j][i]);
         syncTimeDiff[k][j][i]->SetTitle(Form("Run%dTimeDiff%02d%02d",i+startRun,j,k));
 
         // get initial parameters for the dt fit, assume rms = 0.03
@@ -117,35 +116,34 @@ void TimeResolution(){
         rms = 0.03;
 
         // now fit dt, 1st trial
-        fit = new TF1("fit","gaus(0)",mean-3*rms,mean+3*rms);
-        fit->SetParameters(200,mean,0.1);
+        TF1 fit("fit","gaus(0)",mean-3*rms,mean+3*rms);
+        fit.SetParameters(200,mean,0.1);
         syncTimeDiff[k][j][i]->Fit("fit","QMERN");
-        mean = fit->GetParameter(1);
-        rms = fit->GetParameter(2);
-        fit->Clear(); 
-
-        // now fit dt, 2nd trial
-        fit = new TF1("fit","gaus(0)",mean-3*rms,mean+3*rms);
-        fit->SetParameters(200,mean,rms);
+        mean = fit.GetParameter(1);
+        rms = fit.GetParameter(2);
+        
+        fit.SetParameters(200,mean,rms);
         syncTimeDiff[k][j][i]->Fit("fit","QMERN");
-        mean = fit->GetParameter(1);
-        rms = fit->GetParameter(2);
-        fit->Clear(); 
+        mean = fit.GetParameter(1);
+        rms = fit.GetParameter(2);
 
-        // now fit dt, 3rd trial
-        fit = new TF1("fit","gaus(0)",mean-3*rms,mean+3*rms);
-        fit->SetParameters(200,mean,rms);
+        fit.SetParameters(200,mean,rms);
         syncTimeDiff[k][j][i]->Fit("fit","QMERN");
-        mean = fit->GetParameter(1);
-        rms = fit->GetParameter(2);
-        fit->Clear(); 
-
-        cout<<"Run: "<<i+startRun<<", dT("<<j<<","<<k<<"): "<<mean<<"("<<fit->GetChisquare()<<"/"<<fit->GetNDF()<<")"<<endl;
+        mean = fit.GetParameter(1);
+        rms = fit.GetParameter(2);
+        
+        cout<<"    dT("<<j<<","<<k<<"): "<<mean<<" (Chi2/NDF: "<<fit.GetChisquare()<<"/"<<fit.GetNDF()<<")"<<endl;
 
         sigma[k][j][i]=rms;
         effEnergy[k][j][i]=eff(energy[k][i],energy[j][i]);
+      
+        delete syncTimeDiff[k][j][i];
       }
+    
+      delete syncE[j][i];  
     }
+
+    delete f[i];
   }
 
   // now fit the dt vs E_{eff} plots
@@ -166,16 +164,17 @@ void TimeResolution(){
   }
 
   // histograms for fitted C and S terms for same and diff WFD5
-  TH1D *h_C1 = new TH1D("C_term_same", "C_term_same",200,0,0.04); 
-  TH1D *h_C2 = new TH1D("C_term_diff", "C_term_diff",200,0,0.04); 
-  TH1D *h_S1 = new TH1D("S_term_same", "S_term_same",200,0.3,0.55); 
-  TH1D *h_S2 = new TH1D("S_term_diff", "S_term_diff",200,0.3,0.55); 
+  TH1D *h_C1 = new TH1D("C_term_same", "C_term_same",100,0,0.04); 
+  TH1D *h_C2 = new TH1D("C_term_diff", "C_term_diff",100,0,0.04); 
+  TH1D *h_S1 = new TH1D("S_term_same", "S_term_same",100,0.3,0.6); 
+  TH1D *h_S2 = new TH1D("S_term_diff", "S_term_diff",100,0.3,0.6); 
 
-
-  TMultiGraph  *mg  = new TMultiGraph();
+  // define TGraphs and later store them in a TMultiGraph
   TGraph *g[sipmPair.size()];
+  TMultiGraph *mg = new TMultiGraph();
 
   for(int i=0;i<sipmPair.size();i++){
+    
     g[i] = new TGraph(nRun,effEnergy[sipmPair.at(i).first][sipmPair.at(i).second],
         sigma[sipmPair.at(i).first][sipmPair.at(i).second]);
     g[i]->SetMarkerStyle(20);
@@ -186,6 +185,7 @@ void TimeResolution(){
       g[i]->SetMarkerColor(kAzure-sipmPair.at(i).first%9+4);
       myfit->SetLineColor(kAzure-sipmPair.at(i).first%9+4);
     }
+
     else{
       g[i]->SetMarkerColor(kPink-sipmPair.at(i).first%9+4);
       myfit->SetLineColor(kPink-sipmPair.at(i).first%9+4);
@@ -214,6 +214,7 @@ void TimeResolution(){
 
     //leg->AddEntry(g[i],Form("dt(%d,%d),%.4f,%.4f",sipmPair.at(i).first,sipmPair.at(i).second,term1,term2),"p");
     mg->Add(g[i]);
+    g[i]->Clear();
   }
 
   TCanvas *c1 = new TCanvas("c1","c1",1000,600);
@@ -221,7 +222,7 @@ void TimeResolution(){
 
   mg->Draw("AP");
   mg->SetTitle(Form("dt(i,j) vs E_{eff}"));
-  mg->GetYaxis()->SetRangeUser(0.01,0.08);
+  mg->GetYaxis()->SetRangeUser(0.01,0.1);
   mg->GetXaxis()->SetLimits(0,1800);
 
   // leg->SetNColumns(5);
@@ -235,7 +236,9 @@ void TimeResolution(){
   h_C2->SetLineColor(4);
   h_C2->SetLineWidth(2);
   h_C2->Draw();
+  h_C2->GetYaxis()->SetRangeUser(0,1.2*h_C2->GetBinContent(h_C2->GetMaximumBin()));
   h_C2->GetXaxis()->SetTitle("C [c.t.]");
+  h_C2->GetYaxis()->SetTitle("count/0.0004 c.t.");
   h_C2->SetTitle("");
   h_C1->SetLineColor(2);
   h_C1->SetLineWidth(2);
@@ -252,7 +255,9 @@ void TimeResolution(){
   h_S2->SetLineWidth(2);
   h_S2->Draw();
   h_S2->SetTitle("");
+  h_S2->GetYaxis()->SetRangeUser(0,1.2*h_S2->GetBinContent(h_S2->GetMaximumBin()));
   h_S2->GetXaxis()->SetTitle("S [c.t.]");
+  h_S2->GetYaxis()->SetTitle("count/0.003 c.t.");
   h_S1->SetLineColor(2);
   h_S1->SetLineWidth(2);
   h_S1->Draw("same");
@@ -264,6 +269,7 @@ void TimeResolution(){
   legS->Draw();
 
   gStyle->SetOptStat(0);
+
 }
 
 
